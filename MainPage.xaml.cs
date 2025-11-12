e6a98b258643779012979eae6ec00a0976c84e97
Nicolay Neves <deimosdeafrodite4@gmail.com>
2025-11-12 17:28:38 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls;
+namespace NovaVersaoCentralDocs
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCniTapped(object sender, EventArgs e)
+        {
+            // sua lógica aqui
+            DisplayAlert("Ação", "CNI clicado", "OK");
+        }
+
+        // crie os demais handlers similares...
+    }
+}
